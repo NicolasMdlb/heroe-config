ee156c11baf653e479869021a6e0e60621031868
Nicolas Mdlb <nicolas.mdlb@gmail.com>
2022-05-17 01:17:47 +0200

    image and person config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import styles from "../styles/header.module.css"
+import styles from "../styles/header.module.css";
 
 const Header = (props) => {
-    return (
-        <header className={styles.header}>
-           <h1>{props.children}</h1>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <h1>{props.children}</h1>
+    </header>
+  );
 };
 
 export default Header;
