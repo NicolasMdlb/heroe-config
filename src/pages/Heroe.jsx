97d2f8ff5c8324776e5f5d3a27fc1cbcfaaa67a0
Nicolas Mdlb <nicolas.mdlb@gmail.com>
2022-05-18 01:39:51 +0200

    Select perso init caract

--- src/pages/Heroe.jsx
+++ src/pages/Heroe.jsx
@@ -2,21 +2,34 @@ import Header from "./../components/Header";
 import Button from "./../components/Button";
 import Person from "./../components/Configurator/Person";
 import styles from "./../styles/heroe.module.css";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Heroe = () => {
   const titleHeader = "Configurer votre personnage";
-  const [person, setPerson] = useState({image: 1, name: "toto", skills: {agility: 0, brain: 0, power:0}});
+  const [pointAvailable, setPointAvailable] = useState(7);
+  const [person, setPerson] = useState({
+    image: 1,
+    name: "toto",
+    skills: { agility: 0, brain: 0, power: 0 },
+  });
 
-  const handlePerson = () =>{
-
-  }
+  /**
+   * Gestion de la sélection de l'image du personnage !!! att: Aujourd'hui il y a que trois images
+   * @param {number} num 
+   */
+  const imageHandler = (num) => {
+    setPerson(oldPerson => {
+      const newPerson = {...oldPerson}
+      newPerson.image = oldPerson.image + num > 3 ? 1 : oldPerson.image + num < 1 ? 3 : oldPerson.image + num; 
+      return newPerson;
+    });
+  };
 
   return (
     <>
       <Header>{titleHeader}</Header>
       <div className={styles.configurator}>
-        <Person {...person}/>
+        <Person {...person} points={pointAvailable} changeImage={imageHandler} />
         <div>Chois de l'arme</div>
       </div>
       <div className={styles.validPerson}>
