e92bb7c9f14741578cf02005ccdae690bf271a2e
Nicolas Mdlb <nicolas.mdlb@gmail.com>
2022-06-04 00:01:32 +0200

    add/remove skills done

--- src/components/Configurator/skill.jsx
+++ src/components/Configurator/skill.jsx
@@ -3,13 +3,19 @@ import AddCircleTwoToneIcon from "@mui/icons-material/AddCircleTwoTone";
 import styles from "../../styles/skills.module.css";
 
 const Skill = (props) => {
+  let arrows = [];
+
+  for (let index = 0; index < props.points; index++) {
+    arrows.push(<div key={index} className={styles.arrow}></div>);
+  }
+
   return (
     <div className={styles.skills}>
       <strong>{props.children}</strong>
       <div className={styles.skillsPoint}>
-        <RemoveCircleTwoToneIcon onClick={() => props.remove(-1, props.skill)} />
-        {props.points}
-        <AddCircleTwoToneIcon onClick={() => props.add(1, props.skill)} />
+        <RemoveCircleTwoToneIcon onClick={() => props.remove(props.skill)} />
+        <div className={styles.arrows}>{arrows}</div>
+        <AddCircleTwoToneIcon onClick={() => props.add(props.skill)} />
       </div>
     </div>
   );
