b916e94f581265290c58e7b7abbdeb193a65ddb1
Nicolas Mdlb <nicolas.mdlb@gmail.com>
2022-06-06 23:38:16 +0200

    loading desactivate

--- src/pages/Heroes.jsx
+++ src/pages/Heroes.jsx
@@ -5,7 +5,6 @@ import styles from "./../styles/heroes.module.css";
 
 const Heroes = () => {
   const [heroes, setHeroes] = useState([]);
-  const [loading, setLoading] = useState(false);
 
   const fetchHeroes = async () => {
     try {
@@ -13,7 +12,6 @@ const Heroes = () => {
         "https://heroeconfig-default-rtdb.europe-west1.firebasedatabase.app/heroes.json"
       );
       setHeroes(Object.values(responseHeroes));
-      setLoading(false);
     } catch (error) {
       console.error(error.message);
     }
@@ -21,8 +19,6 @@ const Heroes = () => {
 
   // récupère les données sur FireBase dans ce cas.
   useEffect(() => {
-    setLoading(true);
-
     fetchHeroes();
   }, [heroes]);
 
@@ -32,14 +28,10 @@ const Heroes = () => {
         <h2>Liste des héros</h2>
       </header>
       <div className={styles.displayCard}>
-        {loading ? (
-          <p>Chargement...</p>
-        ) : (
-          heroes.length > 0 &&
+        {heroes.length > 0 &&
           heroes.map((heroe, index) => {
             return <HeroeCard key={index} {...heroe} />;
-          })
-        )}
+          })}
       </div>
     </div>
   );
